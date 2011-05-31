d17f93cb17748d2f4d8776670f33c3ecdd855981
Rob McGrail <rob@cwa1498.(none)>
2011-05-31 14:56:18 +1200

    tweaks to handler seeds

--- lib/solr-tweak/seeds/requests.rb
+++ lib/solr-tweak/seeds/requests.rb
@@ -5,29 +5,27 @@ BASELINE = Request.new.seed({
 
 TKI_HANDLER = Request.new.seed({
   :qt => 'tki',
-  :dismax => true,
-  :bq => '-url:/Media/',
 })
 
 TKI_NEWER_APROX = Request.new.seed({
   :dismax => true,
   :qf => 'title_t^10+keyword.text^7+description_t^5+location_t+dc.format+dc.right+dc.subject+topics.all.text+contentprovider+contentsource+strand+learningarea+keylearningobjective+educationalvalue+host+site+institution_name_t+url+ezf_df_text',
   :fq => 'meta_anon_access_b:true AND (url:[* TO *] AND url:http AND -url:cmis.cwa.co.nz AND -url:"http\://admin") AND -keyword:waec AND -keyword:moec AND englishstatus:live',
-  :bq => 'url:.nz^50 (dc.right:Ministry of Education, New Zealand)^100 -url:/Users/ -url:/Media/',
+  :bq => 'url:.nz^50 dc.right:"Ministry of Education, New Zealand"^100',
 })
 
 TKI_DEFAULT = Request.new.seed({
   :dismax => true,
   :qf => 'title_t^10+keyword.text^7+description_t^5+location_t+dc.format+dc.right+dc.subject+topics.all.text+contentprovider+contentsource+strand+learningarea+keylearningobjective+educationalvalue+host+site+institution_name_t+url+ezf_df_text',
   :fq => 'meta_anon_access_b:true AND (url:[* TO *] AND url:http AND -url:cmis.cwa.co.nz AND -url:"http\://admin") AND -keyword:waec AND -keyword:moec AND englishstatus:live',
-  :bq => 'url:.nz^50 (dc.right:Ministry of Education, New Zealand)^100',
+  :bq => 'url:.nz^50 (dc.right:"Ministry of Education, New Zealand)^100',
 })
 
 TKI_BETTER = Request.new.seed({
   :dismax => true,
   :qf => 'title_t^10+keyword.text^7+description_t^5+location_t+dc.format+dc.right+dc.subject+topics.all.text+contentprovider+contentsource+strand+learningarea+keylearningobjective+educationalvalue+host+site+institution_name_t+url+ezf_df_text',
   :fq => 'meta_anon_access_b:true AND (url:[* TO *] AND url:http AND -url:cmis.cwa.co.nz AND -url:"http\://admin") AND -keyword:waec AND -keyword:moec AND englishstatus:live',
-  :bq => 'url:.nz^30 (dc.right:Ministry of Education, New Zealand)^50 -url:/Media/',
+  :bq => 'url:.nz^50 dc.right:"Ministry of Education, New Zealand"^100',
 	:qs => '3',
 	:mm => '-1',
   :pf => 'title_t^30+ezf_df_text^30+description_t^20',
@@ -47,11 +45,8 @@ TKI_FORK = Request.new.seed({
 
 SCRATCH = Request.new.seed({
   :dismax => true,
-  :qf => 'title_t^30+keyword.text^7+description_t^5+location_t+dc.format+dc.right+dc.subject+topics.all.text+contentprovider+contentsource+strand+learningarea+keylearningobjective+educationalvalue+host+site+institution_name_t+url+ezf_df_text',
-  :fq => 'meta_anon_access_b:true AND (url:[* TO *] AND url:http AND -url:cmis.cwa.co.nz AND -url:"http\://admin") AND -keyword:waec AND -keyword:moec AND englishstatus:live',
+  :qf => 'keyword.text+dc.format+dc.right+dc.subject+topics.all.text',
   :bq => '-url:/Media/Images/ AND -url:ezportal-staging',
-  :pf => 'title_t^300+ezf_df_text^100+description_t^60',
-  :ps => '0',
 })
 
 FILTER = Request.new.seed({
